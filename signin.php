047d2d5be3c7de8706dbceed6976d2a81e526f72
Alex Mutuku <81432059+ALEXANDER-KRAZY@users.noreply.github.com>
2022-12-13 07:15:01 +0300

    Create signin.php
    signin

---
+++ signin.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+
+<html lang="en-CA" class="language" style="margin-top:0px !important">
+
+<head>
+
+<title>JointlyIn</title>
+
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="signin.css">
+<style>
+body{
+padding-top:40px;
+padding-bottom:40px;
+background-color:#bcd4e6;
+}
+#back {
+	background:#E5E4E2;
+	}
+.form-signin {
+	max-width:339px;
+	padding:15px;
+	margin:0 auto;
+	}
+.form-signin .form-signin-heading, .form-signin .checkbox {
+	margin-bottom:10px;
+	}
+.form-signin .checkbox {
+	font-weight:normal;
+	}
+.form-signin .form-control {
+	position:relative;
+	height:auto;
+	box-sizing:border-box;
+	padding:10px;
+	font-size:16px;
+	}
+.form-signin .form-control:focus {
+	margin-bottom:-1px;
+	border-bottom-right-radius:0;
+	border-bottom-left-radius:0;
+	}
+.form-signin input{type="password"} {
+margin-bottom:10px;
+border-top-left-radius:0;
+border-top-right-radius:0;
+}
+	
+</style>
+</head>
+        <script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="signin.css">
+            <a class="btn" a href="index.php" id="back">Back Home</a>
+
+
+              <div class="login">
+                <form name="form1" method="post" action="signinquery.php">
+                  
+                  <table width="100%" border="0">
+                  <tr>
+                <td><div align="center"><strong>User Name</strong></td></div>
+                    </tr>
+                    <tr>
+        <td><span id="sprytextfield1">
+                        <label><div align="center">
+                        <input type="text" name="txtUser" id="txtUser">
+</div></label>
+                      <span class="textfieldRequiredMsg">*</span></span>
+          <label></label></td>
+                    </tr>
+                    <tr>
+                   <td><div align="center"><strong>Password</strong></td></div>
+                    </tr>
+                    <tr>
+                <td><span id="sprytextfield2">
+                        <label><div align="center">
+                        <input type="password" name="txtPass" id="txtPass"></div>
+                        </label>
+                      <span class="textfieldRequiredMsg">*</span></span></td>
+                    </tr>
+                    <tr>
+                      <td><div align="center"><strong>User Type</strong></td></div>
+                    </tr>
+                    <tr>
+                    <div align="center"> <td><label><div align="center">
+                        <select name="cmbUser" id="cmbUser">
+                          <option value="JobSeeker">Graduate</option>
+                          <option value="Employer" selected="selected">Employer</option>
+                          <option value="Administrator">Administrator</option>
+                          </select></div>
+                      </label></td></div>
+                    </tr>
+                    <tr>
+                      <td><label>
+                        <div align="center">
+                          <input type="submit" name="button" id="button" value="Login">
+                          </div>
+                      </label></td>
+                    </tr>
+                  </table>
+                  <div align="center"> <a class="btn" a href="signup.php">Dont have an account? Sign Up</a></div>
+      </form>
+              </div>
+        <script type="text/javascript">
+<!--
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1");
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+//-->
+</script>
+
+
+	<div class="icons">
+          <style>
+                    .heading-bg-color-0b13d8cf-04fb-462e-99e5-81591dcb81cc{
+                         background-color:  !important;
+                    }
+          </style>
+<div id="footer">
+            <div id="top" class="noprint"><p><span class="noscreen"></span> <a href="#header" title="Back to top">Back to top<span></span></a></p></div>
+            <hr class="noscreen" />
+        <div class="icons">
+              <style>
+                        .heading-bg-color-0b13d8cf-04fb-462e-99e5-81591dcb81cc{
+                             background-color:  !important;
+                        }
+              </style>
+         <div class="container" id="layout-container-uid0f13">          
+              <div class="socialfollow">
+    <h3><section id="social follow" style="color:tan;">Follow JointlyIn<ul class="list-inline d-inline-block align-middle mb-0"></h3>
+                 <ul class="fb">
+                    <a class="d-inline-block" href="https://www.facebook.com/jointlyin" target="_blank" aria-label="Follow JointlyIn on Facebook, opens in a new tab" data-bi-ecn="Facebook" data-bi-bhvr="126" data-bi-cn="Facebook" data-bi-socchn="Facebook" data-bi-ct="Social Button" data-bi-pa="body" data-bi-compnm="Social Follow - horizontal">
+                        <img src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Facebook%202x?scl=1" alt="Facebook" title="Facebook" width="32" height="32">
+                    </a>
+                 </ul>
+                 <ul class="tw">
+                    <a class="d-inline-block" href="https://twitter.com/jointlyin" target="_blank" aria-label="Follow JointlyIn on Twitter, opens in a new tab" data-bi-ecn="Twitter" data-bi-bhvr="126" data-bi-cn="Twitter" data-bi-socchn="Twitter" data-bi-ct="Social Button" data-bi-pa="body" data-bi-compnm="Social Follow - horizontal">
+                        <img src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Twitter%202x?scl=1" alt="Twitter" title="Twitter" width="32" height="32">
+                    </a>
+                 </ul>
+                 <ul class="in">
+                    <a class="d-inline-block" href="https://www.linkedin.com/company/microsoft" target="_blank" aria-label="Follow JointlyIn on Linkedin, opens in a new tab" data-bi-ecn="LinkedIn" data-bi-bhvr="126" data-bi-cn="LinkedIn" data-bi-socchn="LinkedIn" data-bi-ct="Social Button" data-bi-pa="body" data-bi-compnm="Social Follow - horizontal">
+                        <img src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Linkedin%202x?scl=1" alt="LinkedIn" title="LinkedIn" width="32" height="32">
+                    </a>
+                 </ul>
+              </ul>
+            <script type="application/ld+json">
+            {
+                "@context": "https://schema.org",
+                "@type": "Organization",
+                "name": "JointlyIn",    
+                "sameAs":["https://www.facebook.com/jointlyin","https://twitter.com/jointlyin","https://www.linkedin.com/company/jointlyin"]
+            };
+            </script>
+        </section>    
+    </div>
+    </div>
+    </div>
+    </body>
+    
+    <footer style="color:black;" id="copyright">
+    
+    
+          
+              <center><a href="terms.php">Terms</a></center>
+              <center><a href="privacy.php">Privacy</a></center>
+              <center>Copyright &copy; JointlyIn 2022</center>
+    
+    <p></p>
+    
+    <center>All rights reserved</centre>
+    
+        <nav aria-label="footer" id="copyright">
+        
+          <h3 id="copyright"></h3>
+        </nav>
+    </footer>
